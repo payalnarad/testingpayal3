57272c8d80a063fe6bf540f08af3db76b24e98cb
Samaksh <Samaksh@LAPTOP-UB65FEHH>
2024-09-27 10:56:43 +0530

    By Payal

---
+++ Jenkins_GithubIntegration/src/Jenkins_github/Demo.java
@@ -0,0 +1,35 @@
+package Jenkins_github;
+
+public class Demo 
+{
+	public void m1()
+	{
+	System.out.println("This is m1 Method");	
+
+	}
+
+
+	public void m2()
+	{
+	System.out.println("This is m2 Method");	
+
+	}
+	public void m3()
+	{
+	System.out.println("This is m3 Method");	
+
+	}
+	public void m4()
+	{
+	System.out.println("This is m2 Method");	
+
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World.");
+	}
+
+
+
+}
